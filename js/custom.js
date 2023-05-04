a22f9ca863c996467dee859f38f55fa4371cbb41
trunghieu231 <hieukent215@gmail.com>
2023-05-04 10:01:14 +0700

    update code

---
+++ js/custom.js
@@ -0,0 +1,163 @@
+
+$(document).ready(function()
+{
+
+	var hamb = $('.hamburger');
+	var header = $('.header');
+	var hambActive = false;
+	var menuActive = false;
+	var ctrl = new ScrollMagic.Controller();
+
+	setHeader();
+
+	$(window).on('resize', function()
+	{
+		setHeader();
+	});
+
+	$(document).on('scroll', function()
+	{
+		setHeader();
+	});
+
+	initHeroSlider();
+	initSvg();
+	initHamburger();
+
+	function setHeader()
+	{
+		if(window.innerWidth < 992)
+		{
+			if($(window).scrollTop() > 100)
+			{
+				header.addClass('scrolled');
+			}
+			else
+			{
+				header.removeClass('scrolled');
+			}
+		}
+		else
+		{
+			if($(window).scrollTop() > 100)
+			{
+				header.addClass('scrolled');
+			}
+			else
+			{
+				header.removeClass('scrolled');
+			}
+		}
+		if(window.innerWidth > 991 && menuActive)
+		{
+			closeMenu();
+		}
+	}
+
+	function initHeroSlider()
+	{
+		if($('.hero_slider').length)
+		{
+			var owl = $('.hero_slider');
+
+			owl.owlCarousel(
+			{
+				loop:true,
+				autoplay: true,
+				margin:0,
+				animateOut: 'fadeOut',
+				animateIn: 'fadeIn',
+				nav:false,
+				autoplayHoverPause: false,
+				items: 1,
+			});
+
+
+
+		}	
+	}
+
+	function initSvg()
+	{
+		jQuery('img.svg').each(function()
+		{
+			var $img = jQuery(this);
+			var imgID = $img.attr('id');
+			var imgClass = $img.attr('class');
+			var imgURL = $img.attr('src');
+
+			jQuery.get(imgURL, function(data)
+			{
+				// Get the SVG tag, ignore the rest
+				var $svg = jQuery(data).find('svg');
+
+				// Add replaced image's ID to the new SVG
+				if(typeof imgID !== 'undefined') {
+				$svg = $svg.attr('id', imgID);
+				}
+				// Add replaced image's classes to the new SVG
+				if(typeof imgClass !== 'undefined') {
+				$svg = $svg.attr('class', imgClass+' replaced-svg');
+				}
+
+				// Remove any invalid XML tags as per http://validator.w3.org
+				$svg = $svg.removeAttr('xmlns:a');
+
+				// Replace image with new SVG
+				$img.replaceWith($svg);
+			}, 'xml');
+		});
+	}
+
+	function initHamburger()
+	{
+		if($('.hamburger_container').length)
+		{
+			var hamb = $('.hamburger_container');
+
+			hamb.on('click', function(event)
+			{
+				event.stopPropagation();
+
+				if(!menuActive)
+				{
+					openMenu();
+					
+					$(document).one('click', function cls(e)
+					{
+						if($(e.target).hasClass('menu_mm'))
+						{
+							$(document).one('click', cls);
+						}
+						else
+						{
+							closeMenu();
+						}
+					});
+				}
+				else
+				{
+					$('.menu_container').removeClass('active');
+					menuActive = false;
+				}
+			});
+		}
+	}
+
+	function openMenu()
+	{
+		var fs = $('.menu_container');
+		fs.addClass('active');
+		hambActive = true;
+		menuActive = true;
+	}
+
+	function closeMenu()
+	{
+		var fs = $('.menu_container');
+		fs.removeClass('active');
+		hambActive = false;
+		menuActive = false;
+	}
+
+});
